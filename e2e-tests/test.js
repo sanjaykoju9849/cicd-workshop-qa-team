e08b1a221deb40eac795ccd8a9d33188110108e5
Sanjay <sanjay.koju9849@gmail.com>
2025-07-31 11:15:25 +0545

    Added new test yml file and buildspec file in frontend directory.

---
+++ e2e-tests/test.js
@@ -0,0 +1,296 @@
+{
+  "FHFL-0000-0001-9934": [
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "0.52",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058", //-- fee_amount_code
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "50.00",
+      "remaining_accrued_interest": "0",
+      "remaining_balance": "0.00",
+      "transaction_code": "206", //-- interest_amount_code
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0",
+      "remaining_balance": "100",
+      "transaction_code": "206", //-- interest_amount_code
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208", //--principal_amount_code
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250", //-- fee_amount_code
+      "accrefno": "17780"
+    }
+  ],
+  "FHFL-0000-0001-6960": [
+    {
+      "payment_date": "",  //-- invalid date = should go to missing
+      "payment_amount": "0.0",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "50.00",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250",
+      "accrefno": "17780"
+    }
+  ],
+  "FHFL-0000-0001-5846": [ 
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "0.0",  //-- missing
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "50.00",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250",
+      "accrefno": "17780"
+    }
+  ],
+  "FHFL-0000-0001-9999": [ 
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "100",  
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "-50.00", //--missing
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "408",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250",
+      "accrefno": "17780"
+    }
+  ],
+  "FHFL-0000-0001-9567": [ 
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "100",  
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": null //--missing
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "222", 
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250",
+      "accrefno": "17780"
+    }
+  ],
+  "FHFL-0000-0001-7815": [ 
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "100", 
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": null //--missing
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1046", 
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250",
+      "accrefno": "17780"
+    }
+  ],
+  "FHFL-0000-0001-7815": [ 
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "0", //--missing
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1058",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": null, //missing
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "1046", 
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "143.67",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "206",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "52.09",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "208",
+      "accrefno": "17780"
+    },
+    {
+      "payment_date": "2025-05-16",
+      "payment_amount": "1.25",
+      "remaining_accrued_interest": "0.0039875668",
+      "remaining_balance": "0.00",
+      "transaction_code": "250",
+      "accrefno": "17780"
+    }
+  ]
+}
